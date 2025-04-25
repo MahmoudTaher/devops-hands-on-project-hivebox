2d5da9c409bc30690af49c091e072701b2641af5
mahmoudtaher <mahmoud.taher89@gmail.com>
2025-04-26 00:11:02 +0300

    Check App Version

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM python:3
+WORKDIR /usr/src/app
+COPY main.py ./
+COPY . .
+CMD [ "python", "./main.py" ]
---
+++ main.py
@@ -0,0 +1,6 @@
+# Function to print the HiveBox version
+app_version = "1.0.0"
+def print_app_version():
+    print(f"HiveBox v{app_version}")
+
+print_app_version()
